7867c9c952a15e86c4452da2495758e0812f8aec
javier-cp <43758505+javier-cp6@users.noreply.github.com>
2022-06-01 16:20:49 -0500

    update

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import "bootstrap/dist/css/bootstrap.min.css"
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./styles.css"
 
+import { AuthContextProvider } from "./context/authContext"
+
 import NavigationBar from "./components/NavigationBar";
 import DashboardView from "./views/DashboardView";
 import CategoryView from "./views/CategoryView";
@@ -12,23 +14,24 @@ import AddProductView from "./views/AddProductView";
 import EditProductView from "./views/EditProductView";
 import LoginView from "./views/LoginView";
 
-
 export default function App() {
   return (
     <Router>
-      <NavigationBar />
-      <div className="container mt-4">
-        <Routes>
-          <Route path="/" element={<DashboardView />} />
-          <Route path="/categories" element={<CategoryView />} />
-          <Route path="/addcategory" element={<AddCategoryView />} />
-          <Route path="/editcategory/:catId" element={<EditCategoryView />} />
-          <Route path="/products" element={<ProductView />} />
-          <Route path="/addproduct" element={<AddProductView />} />
-          <Route path="/editproduct/:catId/:prodId" element={<EditProductView />} />
-          <Route path="/login" element={<LoginView />} />
-        </Routes>
-      </div>
+      <AuthContextProvider>
+        <NavigationBar />
+        <div className="container mt-4">
+          <Routes>
+            <Route path="/" element={<DashboardView />} />
+            <Route path="/categories" element={<CategoryView />} />
+            <Route path="/addcategory" element={<AddCategoryView />} />
+            <Route path="/editcategory/:catId" element={<EditCategoryView />} />
+            <Route path="/products" element={<ProductView />} />
+            <Route path="/addproduct" element={<AddProductView />} />
+            <Route path="/editproduct/:catId/:prodId" element={<EditProductView />} />
+            <Route path="/login" element={<LoginView />} />
+          </Routes>
+        </div>
+      </AuthContextProvider>
     </Router>
   )
 }
